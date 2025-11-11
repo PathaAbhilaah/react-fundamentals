c5b003c4a3fae146aa556c39a9a22a4c83db3c42
Abhilash Patha <v-apatha@microsoft.com>
2025-11-11 22:20:14 +0530

    Resolve issues on bgColor application

--- 04BackgroundChangeApp/src/App.jsx
+++ 04BackgroundChangeApp/src/App.jsx
@@ -1,21 +1,42 @@
-import React, {useState} from 'react'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const colors = [
+    "red",
+    "green",
+    "blue",
+    "olive",
+    "gray",
+    "yellow",
+    "pink",
+    "purple",
+    "lavender",
+    "white",
+    "black",
+  ];
+  const [bgcolor, setBgcolor] = useState("white");
 
-  const colors=['red', 'green', 'blue', 'olive', 'gray', 'yellow', 'pink', 'purple', 'lavender', 'white', 'black']
-  const [bgcolor, setBgcolor] = useState('white')
+  React.useEffect(() => {
+    document.body.style.backgroundColor = bgcolor;
+  }, [bgcolor]);
   return (
     <>
-    <div className='container' style={{backgroundColor: bgcolor}}>
-      <div className='colorContainer'>
-        {colors.map((color)=>(
-          <div key={color} style={{backgroundColor: color}} className='eachColor' onClick={() => setBgcolor(color)}>{color}</div>
-        ))}
+      <div className="container">
+        <div className="colorContainer">
+          {colors.map((color) => (
+            <div
+              key={color}
+              className={`eachColor color-${color}`}
+              onClick={() => setBgcolor(color)}
+            >
+              {color}
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
